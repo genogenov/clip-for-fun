4d61b8de3fd4924b9dce0dc06dd4caa73f994908
GGenov <12395610+genogenov@users.noreply.github.com>
2026-02-22 23:55:05 -0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() {
 
     let mut display = WlDisplay::new();
 
-    let mut in_vec = Vec::new();
+    let mut in_vec = Vec::with_capacity(1024);
     stdin().read_to_end(&mut in_vec).unwrap();
 
     let mut registry = display.get_registry(&mut stream).unwrap();
@@ -137,5 +137,4 @@ fn main() {
                 .unwrap();
         }
     }
-    // _ = io::stdout().flush();
 }
